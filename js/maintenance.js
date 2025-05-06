34a5af9561d7a20b7f9f2ea56b9cd894971c4a12
VisAwesme <104176687+VisAwesme@users.noreply.github.com>
2025-05-06 14:30:13 -0400

    fixing thigns

--- js/maintenance.js
+++ js/maintenance.js
@@ -1,6 +1,6 @@
 // only do maintenance mode if like... something major on the site is broken, idfk
 function isMaintenanceMode() {
-  return true; // change this to false to disable maintenance mode
+  return false; // change this to false to disable maintenance mode
 }
 
 if (isMaintenanceMode()) {
